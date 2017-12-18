db2cdd5bc984ec2efb7f94714af790c7914f9ed9
KimiKimi9 <220927@student.pwr.edu.pl>
2017-12-18 15:26:32 +0100

    On branch nie-wiem-  Changes to be committed: 	modified:   package-lock.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGPotv3CrPsj5UKZgkJ4785cwrRwQ79pg',
+  authDomain: 'baza-przestepstw.firebaseapp.com',
+  databaseURL: 'https://baza-przestepstw.firebaseio.com',
+  projectId: 'baza-przestepstw',
+  storageBucket: 'baza-przestepstw.appspot.com',
+  messagingSenderId: '369265722251'
+
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule,
+    FormsModule// And this
   ],
   providers: [],
   bootstrap: [AppComponent]
